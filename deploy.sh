5d65315eec8d8afef8390c0778ab1d9975f46cb4
bk201c <bk201c@outlook.com>
2022-05-09 16:40:44 +0800

    修复部署命令错误

--- deploy.sh
+++ deploy.sh
@@ -11,13 +11,15 @@ cd dist
 
 # 如果你要部署到自定义域名
 # echo 'www.example.com' > CNAME
-
-git checkout master
-git merge dev
-git push
+git init
+git checkout -b main
+git add -A
+git commit -m 'deploy'
 
 # 如果你要部署在 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
 
 # 如果你要部署在 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:BK201c/maptools-web.git master:gh-pages
+git push -f git@github.com:BK201c/maptools-web.git main:gh-pages
+
+cd -
